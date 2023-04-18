df6636769bcfd4fad60ebfc3cb06b8edb44f9e88
hendersonh <hendy@hendersonhood.com>
2023-04-18 16:00:48 -0400

    change incoming port range

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -30,8 +30,8 @@ resource "aws_security_group" "instance" {
     }
 
     ingress {
-    from_port = 22 
-    to_port = 22 
+    from_port = 20 
+    to_port = 29 
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     }
