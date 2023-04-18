4b0052ce29f4cc1a0bc726ca003b2c4c3288aaa2
hendersonh <hendy@hendersonhood.com>
2023-04-18 16:54:49 -0400

    main.tf

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -14,7 +14,7 @@ region = "us-east-2"
 
 resource "aws_key_pair" "upandrunning-key" { 
     key_name = "upandrunning"
-    public_key = file("${path.module}/upandrunning.pub") # ssh-keygen -t ed25519
+    public_key = file("${path.module}/upandrunning.rpub") # ssh-keygen -t ed25519
 }
     
 # Security Group for the Instances
