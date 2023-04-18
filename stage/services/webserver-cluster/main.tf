f0e4c3d3d3625252b2f7c597ee48332015f3d007
hendersonh <hendy@hendersonhood.com>
2023-04-18 16:25:50 -0400

    corrected wrong Key pair name

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -42,7 +42,7 @@ resource "aws_launch_configuration" "example" {
     image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.instance.id]
-    key_name = var.generated_key_name 
+    key_name =  "upandrunning" 
     
     user_data = <<-EOF
         #!/bin/bash
