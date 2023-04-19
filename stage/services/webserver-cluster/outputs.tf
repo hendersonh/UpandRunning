e5a57ec538d74e4d28301fdfed84c6d211ca376b
hendersonh <hendy@hendersonhood.com>
2023-04-18 21:09:54 -0400

    finalize info grap from msql

--- stage/services/webserver-cluster/outputs.tf
+++ stage/services/webserver-cluster/outputs.tf
@@ -3,3 +3,13 @@ output "alb_dns_name" {
   value = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "end_point_of_msql_database" {
+    value = data.terraform_remote_state.mysql.outputs.address
+    description = "End point of database server"
+}
+
+output "data-base-port-number" {
+    value = data.terraform_remote_state.mysql.outputs.port
+    description = "Database port number"
+}
