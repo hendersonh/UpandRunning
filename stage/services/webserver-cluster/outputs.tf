962906ebd84bdf0225dded6a0b2f82b89c5e8b53
hendersonh <hendy@hendersonhood.com>
2023-04-18 20:30:43 -0400

    grap address and port of RDS

--- stage/services/webserver-cluster/outputs.tf
+++ stage/services/webserver-cluster/outputs.tf
@@ -3,3 +3,13 @@ output "alb_dns_name" {
   value = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "mysql_endpoint" {
+  value = "data.terraform_remote_state.mysql.address"
+  description = "The end point of the mysql database"
+}
+    
+output "mysql_port" {
+  value = "data.terraform_remote_state.mysql.port"
+  description = "The port number of the mysql database"
+}
