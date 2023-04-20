f05e0fb141f4dcbc7e76991eb87932865bc921db
hendersonh <hendy@hendersonhood.com>
2023-04-20 19:56:00 -0400

    end of day madness

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -5,6 +5,7 @@ locals {
     any_protocol = "-1"
     tcp_protocol = "tcp"
     all_ips      = ["0.0.0.0/0"]
+    server_port  = 80
 }
 
 
@@ -24,7 +25,8 @@ resource "aws_key_pair" "upandrunning-key" {
     key_name = "upandrunning"
     public_key = file("${path.module}/upandrunning.pub") # ssh-keygen -t ed25519
 }
-    
+
+/*    
 # Security Group for the Instances
 resource "aws_security_group" "instance" {
     name_prefix =   "upandrunning-instance"
@@ -42,23 +44,44 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     }
+}  */
 
+resource "aws_security_group" "instance" {
+    name = "${var.cluster_name}-instance"
+}
+
+resource "aws_security_group_rule" "instance_allow_http_inbound" {
+    type = "ingress"
+    security_group_id = aws_security_group.instance.id 
+
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "instance_allow_all_outbound" {
+    type = "egress"
+    security_group_id = aws_security_group.instance.id
+    from_port = local.any_port
+    to_port = local.any_port
+    protocol = local.any_protocol
+    cidr_blocks = local.all_ips
 }
 
 resource "aws_launch_configuration" "example" {
     name_prefix = "UpandRunning"
     image_id = "ami-0fb653ca2d3203ac1"
-    instance_type = "var.instance_type"
+    instance_type = "${var.instance_type}"
     security_groups = [aws_security_group.instance.id]
     key_name =  "upandrunning" 
     
-    user_data = <<-EOF
-        #!/bin/bash
-        echo "Hello, World" >> index.html
-        echo "${data.terraform_remote_state.mysql.outputs.address}" >> index.html
-        echo "${data.terraform_remote_state.mysql.outputs.port}" >> index.html
-        nohup busybox httpd -f -p ${var.server_port} &
-        EOF  
+    # Render the User Data script as a template 
+    user_data      = templatefile("${path.module}/user-data.tftpl", {
+        db_address = data.terraform_remote_state.mysql.outputs.address  
+        db_port    = data.terraform_remote_state.mysql.outputs.port 
+        server_port = local.server_port 
+    })
   
     # Regquired when using a Lunch configuration with an auto scaling group
     lifecycle {
@@ -136,27 +159,26 @@ resource "aws_lb_listener" "http" {
 #need a security group for ALB
 resource "aws_security_group" "alb" {
     name = "${var.cluster_name}-alb"
-
-    # Allow inbound HTTP requests
-    ingress {
-        from_port   = local.http_port 
-        to_port     = local.http_port 
-        protocol    = "tcp"
-        cidr_blocks = local.all_ips 
-    }
-
-    # Allow all outbound requests
-    egress {
-        from_port   = local.any_port 
-        to_port     = local.any_port 
-        protocol    = local.any_protocol 
-        cidr_blocks = local.all_ips
-    }
-
-    lifecycle {
-      create_before_destroy = true 
-    }
 }
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+    type = "ingress"
+    security_group_id = aws_security_group.alb.id
+    from_port = local.http_port
+    to_port = local.http_port
+    protocol = local.tcp_protocol
+    cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+    type = "egress"
+    security_group_id = aws_security_group.alb.id
+    from_port = local.any_port
+    to_port = local.any_port
+    protocol = local.any_protocol
+    cidr_blocks = local.all_ips
+}
+
 
 #ALG needs a Target group
 resource "aws_lb_target_group" "asg" {
